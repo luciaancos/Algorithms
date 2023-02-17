d576b66fd722e258ad265226a15ef142f92e4b58
Israel Mateos Aparicio <israel4720@gmail.com>
2023-02-17 02:54:28 +0100

    Code style changes

--- game.py
+++ game.py
@@ -1,3 +1,15 @@
+"""
+"Nine men's morris" Game.
+
+This program implements an intelligent agent which knows how to play the
+"Nine men's morris" game against other players by using a randomized algorithm
+and dynamic programming.
+
+Authors:
+    Lucía de Ancos Villa
+    Pablo del Hoyo Abad
+    Israel Mateos Aparicio Ruiz Santa Quiteria
+"""
 import random
 
 from enum import Enum, auto
@@ -11,63 +23,66 @@ BOARD_SIZE = CELLS_PER_RING * RINGS
 class CellState(Enum):
     """Represents the state of a board cell, which can either have a cell from
     some player or be empty."""
+
     WHITE = auto()
     BLACK = auto()
     EMPTY = auto()
 
     def __str__(self) -> str:
+        """Return the string representation of the cell state."""
         match self:
             case CellState.EMPTY:
-                return 'O'
+                return "O"
             case CellState.BLACK:
-                return 'B'
-            case _:
-                return 'W'
+                return "B"
+        return "W"
 
 
 class Board:
-    """ It represents the standard board for the "Nine men's morris" game. It contains
-    three rings, each composed of eight cells. The outer ring is identified with the number 0 and
-    the most inner one, with 2. For the cells, the one at the upper left corner is identified
-    with the number 0. The identifier of the next cells increases by one in clockwise motion, so 
-    the cell to the right of cell zero is one and so on until the last one, which is seven. 
+    """
+    Represents the standard board for the "Nine men's morris" game.
+
+    It contains three rings, each composed of eight cells. The outer ring is
+    identified with the number 0 and the most inner one, with 2. For the cells,
+    the one at the upper left corner is identified with the number 0. The
+    identifier of the next cells increases by one in clockwise motion, so the
+    cell to the right of cell zero is one and so on until the last one, which
+    is seven.
     """
 
     def __init__(self, buff: Optional[list[CellState]] = None):
-        """ buff is a 24 sized list which represents the board. If None is given, an empty board will be generated"""
-
+        """Create an instance of the Board class."""
+        # buff is a 24 sized list which represents the board. If None is given,
+        # an empty board will be generated
         self.buff = buff if buff is not None else [CellState.EMPTY] * BOARD_SIZE
 
         if len(self.buff) > BOARD_SIZE:
             raise ValueError(
-                f"'buff' must have a length of 24, but it has {len(self.buff)}")
+                f"'buff' must have a length of 24, but it has {len(self.buff)}"
+            )
 
     def get_cell(self, ring: int, cell: int) -> CellState:
-        """ Returns the state of a cell located in a specific ring """
-
+        """Return the state of a cell located in a specific ring."""
         idx = self._get_cell_idx(ring, cell)
         return self.buff[idx]
 
     def put_cell(self, ring: int, cell: int, state: CellState):
-        """ Change the state of a cell located in a specific ring. The value of the cell 
-        is completely overriden"""
-
+        """Change the state of a cell located in a specific ring. The value of
+        the cell is completely overriden."""
         idx = self._get_cell_idx(ring, cell)
         self.buff[idx] = state
 
     def remove(self, ring: int, cell: int):
-        """ Equivalent to put_cell(ring, cell, CellState.EMPTY) """
-
+        """Equivalent to put_cell(ring, cell, CellState.EMPTY)."""
         self.put_cell(ring, cell, CellState.EMPTY)
 
     def is_intersection(self, ring: int, cell: int) -> bool:
-        """ Returns whether the cell in a specific has a connection with an inner or outer ring """
-
+        """Return whether the cell in a specific has a connection with an
+        inner or outer ring."""
         return cell % 2 == 1
 
-    def are_adjacent(self, ring1, cell1, ring2, cell2) -> bool:
-        """ Returns true iff the two positions are adjacent """
-
+    def are_adjacent(self, ring1: int, cell1: int, ring2: int, cell2: int) -> bool:
+        """Return true iff the two positions are adjacent."""
         cell_adjacent = abs(cell1 - cell2) in (1, 7)
 
         # if both rings are the same, we only have to focus on the cell
@@ -83,8 +98,7 @@ class Board:
         return False
 
     def is_mill(self, ring: int, cell: int) -> bool:
-        """ Returns true if the piece at (ring, cell) is part of a mill """
-
+        """Return true if the piece at (ring, cell) is part of a mill."""
         current_state = self.get_cell(ring, cell)
 
         # If the given cell is empty, it cannot be part of a mill
@@ -92,54 +106,58 @@ class Board:
             return False
 
         if self.is_intersection(ring, cell):
-            # if it is not one of the corners, the mill can be created with other pieces in the same ring
+            # If it is not one of the corners, the mill can be created with other
+            # pieces in the same ring
             if self._check_same_in_ring(current_state, ring, cell - 1):
                 return True
 
-            # or with pieces in the other rings which are connected to it
+            # Or with pieces in the other rings which are connected to it
             if self._check_across_rings(current_state, cell):
                 return True
 
-        elif self._check_same_in_ring(current_state, ring, cell) or \
-                self._check_same_in_ring(current_state, ring, cell + 6):
-            # if it is a corner, we have to check for two possible mills
+        elif self._check_same_in_ring(
+            current_state, ring, cell
+        ) or self._check_same_in_ring(current_state, ring, cell + 6):
+            # If it is a corner, we have to check for two possible mills
             return True
 
         return False
 
-    def _check_same_in_ring(self, check_state: CellState, ring, cell) -> bool:
-        """ Checks whether there is a mill starting at a corner. It is checked in clockwise motion 
-        NOTE: this method does NOT check whether the cell is a corner """
-
+    def _check_same_in_ring(self, check_state: CellState, ring: int, cell: int) -> bool:
+        """Check whether there is a mill starting at a corner. It is checked
+        in clockwise motion.
+        NOTE: This method does NOT check whether the cell is a corner."""
         for i in range(3):
             if check_state != self.get_cell(ring, (cell + i) % CELLS_PER_RING):
                 return False
 
         return True
 
-    def _check_across_rings(self, check_state, cell) -> bool:
-        """ Checks whether there is a mill across ring which contains cell.
-        NOTE: this method does NOT check whether a cell is in a intersection"""
-
+    def _check_across_rings(self, check_state: CellState, cell: int) -> bool:
+        """Check whether there is a mill across ring which contains cell.
+        NOTE: This method does NOT check whether a cell is in a intersection."""
         for ring in range(RINGS):
             if check_state != self.get_cell(ring, cell):
                 return False
 
         return True
 
-    def _get_cell_idx(self, ring: int, cell: int):
-        """Returns the index in the board from a given cell."""
+    def _get_cell_idx(self, ring: int, cell: int) -> int:
+        """Return the index in the board from a given cell."""
         if ring < 0 or ring >= RINGS:
             raise ValueError(
-                f"The ring must be between 0 and {RINGS}, but {ring} was given")
+                f"The ring must be between 0 and {RINGS}, but {ring} was given"
+            )
 
         if cell < 0 or cell >= CELLS_PER_RING:
             raise ValueError(
-                f"The cell must be between 0 and {CELLS_PER_RING}, but {cell} was given")
+                f"The cell must be between 0 and {CELLS_PER_RING}, but {cell} was given"
+            )
 
         return ring * CELLS_PER_RING + cell
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return the string representation of the board."""
         table = f"{self.buff[0]}----------------"
         table += f"{self.buff[1]}----------------"
         table += f"{self.buff[2]}\n"
@@ -186,6 +204,7 @@ class Board:
 
 class GameMode(Enum):
     """Represents the mode in which the game is in a given moment."""
+
     PLACE = auto()
     MOVE = auto()
     DELETE = auto()
@@ -194,44 +213,53 @@ class GameMode(Enum):
 class Turn(Enum):
     """Represents the turn of the game in a given moment, that is, who is the
     active player."""
+
     WHITE = 1
     BLACK = 2
 
 
 class MillGame:
-    """ This class contains the game logic """
+    """
+    Class which contains the game logic.
+
+    In the initial stage, each player places his/her chips alternatively, which
+    is achieved with the attribute 'turn'. At any moment, if the active player
+    forms a mill, that is, three chips in adjacent/connected cells, he/she is
+    allowed to remove a chip placed on the board by the other player. Once all
+    chips of a player are placed on the board, he/she can only move his/her
+    chips to adjacent cells. The player who has two chips left on the board loses.
+    """
 
     def __init__(self, turn: Optional[Turn] = None):
-        """ turn is the turn of the one who starts the game. If None is given, it is chosen randomly """
-
+        """Create an instance of the MillGame class."""
+        # turn is the turn of the one who starts the game. If None is given,
+        # it is chosen randomly
         self.turn = turn if self.turn is not None else Turn(random.randint(1, 2))
         self.mode = GameMode.PLACE
         self.board = Board()
 
     def place(self, ring: int, cell: int) -> bool:
-        """Places the next chip of the active player in a cell of the board."""
-        if self.mode != GameMode.PLACE or self.board.get_cell(ring, cell) != CellState.EMPTY:
+        """Place the next chip of the active player in a cell of the board."""
+        if (
+            self.mode != GameMode.PLACE
+            or self.board.get_cell(ring, cell) != CellState.EMPTY
+        ):
             return False
 
         self.board.put_cell(ring, cell, self._get_state_by_turn())
 
         return True
 
-
     def move(self, ring1: int, cell1: int, ring2: int, cell2: int):
-        """Moves a chip of the active player placed in the board to another cell."""
+        """Move a chip of the active player placed in the board to another cell."""
         if self.mode != GameMode.MOVE:
-            raise ValueError(
-                "The game mode must be 'MOVE'")
+            raise ValueError("The game mode must be 'MOVE'")
         if not self.board.are_adjacent(ring1, cell1, ring2, cell2):
-            raise ValueError(
-                "You can only move your chips to adjacent cells")
+            raise ValueError("You can only move your chips to adjacent cells")
         if self.turn != self.board.get_cell(ring1, cell1):
-            raise ValueError(
-                "You can only move your own chips")
+            raise ValueError("You can only move your own chips")
         if self.board.get_cell(ring2, cell2) != CellState.EMPTY:
-            raise ValueError(
-                "The new position of the chip must be empty")
+            raise ValueError("The new position of the chip must be empty")
 
         self.board.remove(ring1, cell1)
         self.board.put_cell(ring2, cell2, self._get_state_by_turn())
@@ -240,22 +268,21 @@ class MillGame:
             self.mode = GameMode.DELETE
 
     def remove(self, ring: int, cell: int):
-        """Removes a cell from the board permanently."""
+        """Remove a cell from the board permanently."""
         if self.mode != GameMode.DELETE:
-            raise ValueError(
-                "The game mode must be 'DELETE'")
+            raise ValueError("The game mode must be 'DELETE'")
         if self.board.is_mill(ring, cell):
-            raise ValueError(
-                "You cannot remove chips belonging to a mill")
+            raise ValueError("You cannot remove chips belonging to a mill")
 
         self.board.remove(ring, cell)
 
     def _get_state_by_turn(self) -> CellState:
-        """Returns the state of cells owned by the active player."""
+        """Return the state of cells owned by the active player."""
         return CellState.BLACK if self.turn == Turn.BLACK else CellState.WHITE
 
 
 class State:
+    # TODO: Implement
     pass
 
 
