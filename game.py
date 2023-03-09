490f85546b928ef85a991245cd9d233a9a66677c
Pablo del Hoyo Abad <pablo.hoyo@alu.uclm.es>
2023-03-09 19:30:06 +0100

    Implement method 'apply_move() to MillGame'

--- game.py
+++ game.py
@@ -11,8 +11,10 @@ Authors:
     Israel Mateos Aparicio Ruiz Santa Quiteria
 """
 
-import random
+from __future__ import annotations
 
+import random
+import json
 from enum import Enum, auto
 from dataclasses import dataclass, field
 from typing import Optional
@@ -55,6 +57,44 @@ class Player:
     alive_pieces: int = field(default=NUM_PIECES_PER_PLAYER)
 
 
+@dataclass
+class Move:
+    """ Represents the move which has to be made to move a MillGame from one state to another
+    Notice that the action of killing a piece is merged with the action which triggered it. In case
+    no piece were killed, then kill is None. 
+    """
+
+    # The (ring, cell) where you are placing a piece
+    next_pos: tuple[int, int]
+
+    # The (ring, cell) where the piece was previously. If it were not on the board before,
+    # this value is None.
+    pos_init: Optional[tuple[int, int]] = None
+
+    # The (ring, cell) of the piece which has been deleted. If no piece were killed, this
+    # value is None.
+    kill: Optional[tuple[int, int]] = None
+
+    @classmethod
+    def from_json(cls, json_str: str) -> Move:
+        data = json.loads(json_str)
+        return cls(
+            data["NEXT_POS"],
+            None if data["POS_INIT"] == -1 else (data["POS_INIT"] // 8, data["POS_INIT"] % 8),
+            None if data["KILL"] == -1 else (data["KILL"] // 8, data["KILL"] % 8)
+        )
+
+    def to_json(self) -> str:
+        return json.dumps({
+            "POS_INIT": -1 if self.pos_init is None else self.pos_init[0] * 8 + self.pos_init[1] + 8,
+            "NEXT_POS": self.next_pos[0] * 8 + self.next_pos[1] + 8,
+            "KILL": -1 if self.kill is None else self.kill[0] * 8 + self.kill[1] + 8
+        })
+
+    def __str__(self) -> str:
+        return f"<move>={{'POS_INIT':{self.pos_init}, 'NEXT_POS':{self.next_pos},'KILL':{self.kill}}}"
+
+
 class MillGame:
     """
     Class which contains the game logic.
@@ -77,13 +117,134 @@ class MillGame:
 
         self._players = [Player(CellState.WHITE), Player(CellState.BLACK)]
 
-    def place(self, ring: int, cell: int):
-        """Place the next chip of the active player in a cell of the board."""
+    def apply_move(self, move: Move):
+        """ Apply the given move """
+
+        if move.pos_init is None:
+            if move.kill is None:
+                self.place(*move.next_pos)
+            else:
+                self.place_and_remove(*move.next_pos, *move.kill)
+        else:
+            if move.kill is None:
+                self.move(*move.pos_init, *move.next_pos)
+            else:
+                self.move_and_remove(*move.pos_init, *move.next_pos, *move.kill)
+
+    def check_place(self, ring: int, cell: int):
+        """ Checks whether the current player can place a piece in (ring, cell) """
+
         if not self._check_mode(GameMode.PLACE):
             raise InvalidStateException("The game mode must be 'PLACE'")
 
         if self.board.get_cell(ring, cell) != CellState.EMPTY:
             raise InvalidMoveException("The cell is not empty")
+
+    def check_move(self, ring1: int, cell1: int, ring2: int, cell2: int):
+        """ Checks whether the current player can mova a piece from (ring1, cell1) 
+        to (ring2, cell2) """
+
+        if not self._check_mode(GameMode.MOVE):
+            raise InvalidStateException("The game mode must be 'MOVE'")
+
+        if not self.board.are_adjacent(ring1, cell1, ring2, cell2):
+            raise InvalidMoveException(
+                "You can only move the pieces to adjacent cells")
+
+        if self.current_player().associated_cell_state != self.board.get_cell(ring1, cell1):
+            raise InvalidMoveException("You can only move your own pieces")
+
+        if self.board.get_cell(ring2, cell2) != CellState.EMPTY:
+            raise InvalidMoveException(
+                "The new position of the piece must be empty")
+
+    def check_remove(self, ring: int, cell: int):
+        """ Checks whether the current player can remove a piece from (ring, cell) """
+
+        if not self.has_to_delete:
+            raise InvalidStateException("It is not required to remove a piece")
+
+        if self.board.get_cell(ring, cell) == CellState.EMPTY:
+            raise InvalidMoveException(
+                "It is not possible to remove and empty cell")
+
+        if self.board.get_cell(ring, cell) != self.other_player().associated_cell_state:
+            raise InvalidMoveException(
+                "You cannot remove chips which belong to the current player")
+
+        if self.board.is_mill(ring, cell) and not self.all_pieces_form_mill(self.other_player()):
+            raise InvalidMoveException(
+                "You cannot remove chips belonging to a mill when there are chips which don't belong to any of them")
+
+    def place_and_remove(self, ring: int, cell: int, rem_ring: int, rem_cell: int):
+        """ Place a piece and remove another one. This method either suceeds or fails. If it suceeds,
+        the game is updated. Otherwise, an exception is raised and the state is left as it was.
+        If this method is called when place was needed but removing not, then an 
+        InvalidMoveException is raised """
+
+        self.check_place(ring, cell)
+
+        self.board.put_cell(
+            ring, cell, self.current_player().associated_cell_state)
+
+        if self.board.is_mill(ring, cell):
+            self.has_to_delete = True
+            try:
+                self.remove(rem_ring, rem_cell)
+            except (ValueError, InvalidMoveException) as exc:
+                # Undo the call to put_cell
+                self.board.remove(ring, cell)
+                self.has_to_delete = False
+                raise exc
+        else:
+            # Undo the call to put cell
+            self.board.remove(ring, cell)
+            raise InvalidMoveException(
+                "The place action was correct but not the remove one")
+
+        self.current_player().remaining_pieces -= 1
+        if self.current_player().remaining_pieces == 0 and self.other_player().remaining_pieces == 0:
+            self.mode = GameMode.MOVE
+
+        self._change_turn()
+
+    def move_and_remove(self, ring1: int, cell1: int, ring2: int, cell2: int, rem_ring: int, rem_cell: int):
+        """ Move a piece and remove another one. This method either suceeds or fails. If it suceeds,
+        the game is updated. Otherwise, an exception is raised and the state is left as it was.
+        If this method is called when move was needed but removing not, then an 
+        InvalidMoveException exception is raised """
+
+        def undo_move():
+            self.board.remove(ring2, cell2)
+            self.board.put_cell(ring1, cell1, self.current_player().associated_cell_state)
+
+        self.check_move(ring1, cell1, ring2, cell2)
+
+        self.board.remove(ring1, cell1)
+        self.board.put_cell(
+            ring2, cell2, self.current_player().associated_cell_state)
+
+        if self.board.is_mill(ring2, cell2):
+            self.has_to_delete = True
+            try:
+                self.remove(rem_ring, rem_cell)
+            except (ValueError, InvalidMoveException) as exc:
+                # Undo the call to remove and put_cell
+                undo_move()
+                self.has_to_delete = False
+                raise exc
+        else:
+            # Undo the call to remove and put_cell
+            undo_move()
+            raise InvalidMoveException(
+                "The move action was correct but not the remove one")
+
+        self._change_turn()
+
+    def place(self, ring: int, cell: int):
+        """Place the next chip of the active player in a cell of the board."""
+
+        self.check_place(ring, cell)
 
         self.board.put_cell(
             ring, cell, self.current_player().associated_cell_state)
@@ -100,19 +261,8 @@ class MillGame:
 
     def move(self, ring1: int, cell1: int, ring2: int, cell2: int):
         """Move a chip of the active player placed in the board to another cell."""
-        if not self._check_mode(GameMode.MOVE):
-            raise InvalidStateException("The game mode must be 'MOVE'")
 
-        if not self.board.are_adjacent(ring1, cell1, ring2, cell2):
-            raise InvalidMoveException(
-                "You can only move the pieces to adjacent cells")
-
-        if self.current_player().associated_cell_state != self.board.get_cell(ring1, cell1):
-            raise InvalidMoveException("You can only move your own pieces")
-
-        if self.board.get_cell(ring2, cell2) != CellState.EMPTY:
-            raise InvalidMoveException(
-                "The new position of the piece must be empty")
+        self.check_move(ring1, cell1, ring2, cell2)
 
         self.board.remove(ring1, cell1)
         self.board.put_cell(
@@ -123,22 +273,10 @@ class MillGame:
         else:
             self._change_turn()
 
-    def remove(self, ring, cell):
+    def remove(self, ring: int, cell: int):
         """Remove a cell from the board permanently."""
-        if not self.has_to_delete:
-            raise InvalidStateException("It is not required to remove a piece")
 
-        if self.board.get_cell(ring, cell) == CellState.EMPTY:
-            raise InvalidMoveException(
-                "It is not possible to remove and empty cell")
-
-        if self.board.get_cell(ring, cell) != self.other_player().associated_cell_state:
-            raise InvalidMoveException(
-                "You cannot remove chips which belong to the current player")
-
-        if self.board.is_mill(ring, cell) and not self.all_pieces_form_mill(self.other_player()):
-            raise InvalidMoveException(
-                "You cannot remove chips belonging to a mill when there are chips which don't belong to any of them")
+        self.check_remove(ring, cell)
 
         self.board.remove(ring, cell)
         self.other_player().alive_pieces -= 1
@@ -267,7 +405,7 @@ class MillGame:
             or self.turn != state_dict["TURN"]
             or self._players[0].remaining_pieces != state_dict["CHIPS"][0]
             or self._players[1].remaining_pieces != state_dict["CHIPS"][1]
-            ):
+        ):
             return False
         return True
 
--- state.py
+++ state.py
@@ -1,44 +1,32 @@
 from __future__ import annotations
 import copy
 from dataclasses import dataclass
+from typing import Optional
 from game import GameMode, InvalidMoveException, InvalidStateException, MillGame, CellState
+import json
 
 
-@dataclass
-class Move: 
-    pos_init: int
-    next_pos: int
-    kill: int
 
-    def __str__(self) -> str:
-        return f"<move>={{'POS_INIT':{self.pos_init}, 'NEXT_POS':{self.next_pos},'KILL':{self.kill}}}"
 
-@dataclass
-class Sucesor:
-    state : State
-    move: Move
-    next_state: State
+# @dataclass
+# class Sucesor:
+    # state: State
+    # move: Move
+    # next_state: State
 
-    def __str__(self) -> str:
-        return f"<sucesor>={{'STATE':{self.state},'MOVE':{self.move},'NEXT_STATE':{self.next_state}}}"
-# TODO: Action has a move, and Action.sucesor has it again?
-@dataclass
-class Action:
-    move: Move
-    sucesor: Sucesor
+    # def __str__(self) -> str:
+        # return f"<sucesor>={{'STATE':{self.state},'MOVE':{self.move},'NEXT_STATE':{self.next_state}}}"
 
-    def __str__(self) -> str:
-        return f"{self.move}, {self.sucesor}"
 
 class State:
-    
+
     def __init__(self,
                  game: MillGame):
         self.game = game
-        self.free = [] 
-        self.gamer = [[],[]]
+        self.free = []
+        self.gamer = [[], []]
         self.turn = game.turn
-        self.chips = [[],[]]
+        self.chips = [[], []]
 
         for i in range(24):
             if self.game.board.buff[i] == CellState.EMPTY:
@@ -62,65 +50,66 @@ class State:
         for free_chip in self.free:
             game_copy = copy.deepcopy(self.game)
             try:
-                game_copy.place(free_chip//8,free_chip%8)
-            except(ValueError, InvalidMoveException, InvalidStateException):
+                game_copy.place(free_chip//8, free_chip % 8)
+            except (ValueError, InvalidMoveException, InvalidStateException):
                 continue
-            
+
             if game_copy.has_to_delete:
                 for opponent_chip in self.gamer[self.turn.value]:
                     try:
-                        game_copy.remove(opponent_chip//8, opponent_chip%8)
-                    except(ValueError, InvalidMoveException, InvalidStateException):
+                        game_copy.remove(opponent_chip//8, opponent_chip % 8)
+                    except (ValueError, InvalidMoveException, InvalidStateException):
                         continue
                     next_state = State(game_copy)
-                    move = Move(-1,free_chip, opponent_chip)
-                    sucessor = Sucesor(self, move,next_state)
+                    move = Move(-1, free_chip, opponent_chip)
+                    sucessor = Sucesor(self, move, next_state)
                     action = Action(move, sucessor)
                     successors.append(action)
             else:
                 next_state = State(game_copy)
-                move = Move(-1,free_chip, -1)           
-                sucessor = Sucesor(self, move,next_state)
+                move = Move(-1, free_chip, -1)
+                sucessor = Sucesor(self, move, next_state)
                 action = Action(move, sucessor)
                 successors.append(action)
         return successors
 
     def generate_succ_move(self, successors: list[Action]) -> list[Action]:
         for player_chip in self.gamer[1 - self.turn.value]:
-                for free_chip in self.free:
-                    game_copy = copy.deepcopy(self.game)
-                    try:
-                        game_copy.move(player_chip//8,player_chip%8,free_chip//8,free_chip%8)
-                    except(ValueError, InvalidMoveException, InvalidStateException):
-                        continue
-                    if game_copy.has_to_delete:
-                        for opponent_chip in self.chips[self.turn.value]:
-                            try:
-                                game_copy.remove(opponent_chip//8, opponent_chip%8)
-                            except(ValueError, InvalidMoveException, InvalidStateException):
-                                continue
-                            next_state = State(game_copy)
-                            move = Move(player_chip,free_chip, opponent_chip)
-                            sucessor = Sucesor(self, move, next_state)
-                            action = Action(move, sucessor)
-                            successors.append(action)
-                    else:
+            for free_chip in self.free:
+                game_copy = copy.deepcopy(self.game)
+                try:
+                    game_copy.move(player_chip//8, player_chip %
+                                   8, free_chip//8, free_chip % 8)
+                except (ValueError, InvalidMoveException, InvalidStateException):
+                    continue
+                if game_copy.has_to_delete:
+                    for opponent_chip in self.chips[self.turn.value]:
+                        try:
+                            game_copy.remove(opponent_chip//8,
+                                             opponent_chip % 8)
+                        except (ValueError, InvalidMoveException, InvalidStateException):
+                            continue
                         next_state = State(game_copy)
-                        move = Move(player_chip,free_chip, -1)                     
+                        move = Move(player_chip, free_chip, opponent_chip)
                         sucessor = Sucesor(self, move, next_state)
                         action = Action(move, sucessor)
                         successors.append(action)
-
+                else:
+                    next_state = State(game_copy)
+                    move = Move(player_chip, free_chip, -1)
+                    sucessor = Sucesor(self, move, next_state)
+                    action = Action(move, sucessor)
+                    successors.append(action)
 
     def successors(self) -> list[Action]:
         successors = []
-        
+
         if self.game.mode == GameMode.PLACE:
             successors = self.generate_succ_place(successors)
-                                
+
         if self.game.mode == GameMode.MOVE:
             successors = self.generate_succ_move(successors)
-    
+
         return successors
 
     def __str__(self) -> str:
@@ -128,3 +117,4 @@ class State:
         joined_gamers = ",".join(map(str, self.gamer))
         joined_chips = ",".join(map(str, self.chips))
         return f"<state>={{'FREE':[ {joined_free} ],'GAMER':[{joined_gamers}],'TURN':{self.turn.name},'CHIPS':{joined_chips}}}"
+
